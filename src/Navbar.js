dca5adf54b6c2bdbfe8003439bc24bf24a855872
Shahzaib Ekram <shahzebekram2468@gmail.com>
2022-08-19 02:30:08 +0500

    DAO Surge

---
+++ src/Navbar.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import './App.css';
+
+export default function Navbar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            DAO Contract
+          </Typography>
+          <Button variant="outlined" className='walletButton'>Connect Wallet</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
