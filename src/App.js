d5cb5813aab90c6e5178db15e588ab985d1ba73c
Shahzaib Ekram <shahzebekram2468@gmail.com>
2022-08-19 17:39:26 +0500

    Removing LOGO

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import { Routes, Route, } from "react-router-dom";
 import './App.css';
 import Navbar from './Navbar'
 import Proposal from './Proposal'
@@ -11,8 +10,6 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Proposal />} />
-        {/* <Route path="expenses" element={<Expenses />} />
-      <Route path="invoices" element={<Invoices />} /> */}
       </Routes>
     </>
   );
